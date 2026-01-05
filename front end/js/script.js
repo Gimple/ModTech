dc15b79a3aa778fa8235d1533ff53a929b9ead9c
Zeldrickjesus <143236106+zjdelossantos@users.noreply.github.com>
2026-01-05 16:52:55 +0800

    Delete front end directory

--- front end/js/script.js
+++
@@ -1,9 +0,0 @@
-const sendBtn = document.getElementById("sendBtn");
-const input = document.getElementById("userInput");
-
-sendBtn.addEventListener("click", () => {
-    if (input.value.trim() !== "") {
-        alert("You typed: " + input.value);
-        input.value = "";
-    }
-});
